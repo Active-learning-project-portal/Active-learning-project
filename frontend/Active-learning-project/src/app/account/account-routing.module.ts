bcf184387712ee778b1c81883ee5e286f4a6ddaf
Tshimangadzo Mudzanani <mudzanani27@gmail.com>
2023-02-08 14:15:36 +0200

    setted up netlify

---
+++ frontend/Active-learning-project/src/app/account/account-routing.module.ts
@@ -0,0 +1,21 @@
+import { SignInComponent } from './signin/signin.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+
+const routes: Routes = [
+  {
+    path: 'signin',
+    component: SignInComponent,
+  },
+  {
+    path: 'signup',
+    component: SignupComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AccountRoutingModule { }
