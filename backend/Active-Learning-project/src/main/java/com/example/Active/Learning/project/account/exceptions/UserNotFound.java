e2576fe085da753c4b6ffc627008b8c8495952f7
Tshimangadzo Mudzanani <mudzanani27@gmail.com>
2023-02-13 12:38:24 +0200

    fixed backend naming convention

--- backend/Active-Learning-project/src/main/java/com/example/Active/Learning/project/account/exceptions/UserNotFound.java
+++
@@ -1,8 +0,0 @@
-package com.example.Active.Learning.project.account.exceptions;
-
-public class UserNotFound extends  RuntimeException{
-    public UserNotFound(final String errorMessage){
-        super(errorMessage);
-    }
-
-}
