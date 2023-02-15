5a91a93f0c99f4b2b276ff4ac8da233f036a5e93
Tshimangadzo <mudzanani27@gmail.com>
2023-02-15 22:46:17 +0200

    changed message response

--- backend/Active-Learning-project/src/main/java/com/example/Active/Learning/project/account/payload/response/MessageResponse.java
+++ backend/Active-Learning-project/src/main/java/com/example/Active/Learning/project/account/payload/response/MessageResponse.java
@@ -1,7 +1,7 @@
 package com.example.Active.Learning.project.account.payload.response;
 
 public class MessageResponse extends RuntimeException{
-    public static final String EMAIL_ALREADY_EXISTS = "Username is already taken!";
+    public static final String EMAIL_ALREADY_EXISTS = "Email already in use";
     public static final String ROLE_NOT_FOUND_ERROR ="Role is not found.";
     public static final String USER_CREATED_SUCCESSFULLY = "User registered successfully!";
     public static final String USER_NOT_FOUND = "User not Found!";
