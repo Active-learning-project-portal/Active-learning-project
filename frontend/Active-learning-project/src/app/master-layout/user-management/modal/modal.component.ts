564c1de7360be368322b8db8777dc24b5f28bdd2
Tshimangadzo <mudzanani27@gmail.com>
2023-02-19 20:25:24 +0200

    added fake user management

--- frontend/Active-learning-project/src/app/master-layout/user-management/modal/modal.component.ts
+++ frontend/Active-learning-project/src/app/master-layout/user-management/modal/modal.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { UsersList } from '../models/user-list.interface';
 
 @Component({
   selector: 'alp-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css'],
 })
-export class ModalComponent {
+export class ModalComponent implements OnInit {
+  @Input() btnType!: 'Deactivate' | 'Activate' | 'Update' | 'View';
+  @Input() user!: UsersList;
 
+  ngOnInit(): void {
+   console.log("I am child")
+  }
 }
--- frontend/Active-learning-project/src/app/master-layout/user-management/user-management.component.ts
+++ frontend/Active-learning-project/src/app/master-layout/user-management/user-management.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog} from '@angular/material/dialog';
 import { FakeUsers } from './models/fake-users';
 import { UsersList } from './models/user-list.interface';
 
@@ -9,8 +10,43 @@ import { UsersList } from './models/user-list.interface';
 })
 export class UserManagementComponent implements OnInit {
   users!: UsersList[];
+  allCount!:number;
+  activeCount!:number;
+  deactiveCount!:number;
 
+  constructor() {}
+
+  
   ngOnInit() {
+    this.filterAllUsers();
+    this.allCount = this.allUsers;
+    this.activeCount = this.activeUsers.length;
+    this.deactiveCount = this.deactiveUsers.length;
+  }
+
+  get activeUsers():UsersList[]{
+    return FakeUsers.filter(user => user.action === "Activate");
+  }
+
+  get deactiveUsers():UsersList[]{
+    return FakeUsers.filter(user => user.action === "Deactivate");
+  }
+
+  get allUsers():number{
+    return FakeUsers.length;
+  }
+
+  filterActivateUsers():void{
+    this.users = this.activeUsers;
+  }
+
+  filterDeactivateUsers():void{
+    this.users = this.deactiveUsers;
+  }
+  filterAllUsers():void{
     this.users = FakeUsers;
   }
+
+
+
 }
