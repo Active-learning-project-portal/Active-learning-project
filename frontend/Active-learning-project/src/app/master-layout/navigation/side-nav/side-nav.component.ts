ab4fc88471ff0277ba1e9e1753802214db1d08ca
Silas <u15231748@tuks.co.za>
2023-02-21 13:29:27 +0200

    Basic Master Layout

---
+++ frontend/Active-learning-project/src/app/master-layout/navigation/side-nav/side-nav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'alp-side-nav',
+  templateUrl: './side-nav.component.html',
+  styleUrls: ['./side-nav.component.css']
+})
+export class SideNavComponent {
+
+}
