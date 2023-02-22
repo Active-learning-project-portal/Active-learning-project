beecdf94863dedb013fbdd574805daab1e606118
Tshimangadzo Mudzanani <mudzanani27@gmail.com>
2023-02-22 10:20:20 +0200

    sving

--- frontend/Active-learning-project/src/app/master-layout/master-layout.module.ts
+++ frontend/Active-learning-project/src/app/master-layout/master-layout.module.ts
@@ -3,8 +3,6 @@ import {NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MasterLayoutRoutingModule } from './master-layout-routing.module';
 import { MasterLayoutComponent } from './master-layout.component';
-import { SideNavComponent } from '../shared/components/side-nav/side-nav.component';
-import { TopNavComponent } from '../shared/components/top-nav/top-nav.component';
 import { ModalComponent } from './user-management/modal/modal.component';
 import { RemoveRoleAndUnderScorePipe } from '../shared/pipes/remove-role-and-under-score.pipe';import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { TopNavComponent } from './navigation/top-nav/top-nav.component';
